0a1e41e08e4851ce67122d3f36e94014ef0d41a0
josue-acosta <hannibalsway001@gmail.com>
2020-01-23 01:00:20 -0600

    added accordion

--- js/init-materialize.js
+++ js/init-materialize.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+});
+
